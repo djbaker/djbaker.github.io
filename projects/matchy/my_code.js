95ac35b924574fbccd4249f2f60db3d0b405bd78
Daniel Baker <djbaker1.007@gmail.com>
2017-03-28 00:05:10 +0000

    part 1 of matchy

--- projects/matchy/my_code.js
+++ projects/matchy/my_code.js
@@ -1,1 +1,44 @@
+var animal = {};
+animal.species = 'dog';
+animal.name = 'joey';
+animal.noises = [];
+console.log(animal);
 
+var noises = [];
+noises[0] = 'bark';
+noises[1] = 'growl';
+noises.unshift('yip');
+noises[noises.length] = 'bark bark';
+console.log(noises.length);
+console.log(noises[noises.length - 1]);
+console.log(noises);
+
+var animals = [];
+animal['noises'] = noises;
+animals.push(animal);
+console.log(animals);
+
+var duck = {
+   species: 'duck',
+   name: 'Jerome',
+   noises: ['quack', 'honk', 'sneeze', 'woosh']
+};
+animals.push(duck);
+console.log(animals);
+
+var cat = {
+    species: 'cat',
+    name: 'Sarah',
+    noises: ['meow', 'purr', 'screech','hiss']
+}
+
+var friends = [];
+// We picked an array because we can make an ordered list of friends that we can loop over
+function randomM() {
+let index = Math.floor(Math.random() * animals.length - 1);
+return animals[index];
+}
+
+friends.push(randomM()["name"]);
+
+console.log(friends)
