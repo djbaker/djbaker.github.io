a2072d7d9fed7828afaa506e9d6c181f53a4c923
Daniel Baker <djbaker1.007@gmail.com>
2017-03-30 00:19:48 +0000

    first function-master commit

--- projects/function-master/function-master.js
+++ projects/function-master/function-master.js
@@ -2,7 +2,25 @@
 function objectValues(object) {
     var arr = [];
     for (var key in object) {
-        arr.push(object.key);
+        arr.push(object[key]);
     }
     return arr;
+}
+
+function keysToString(object) {
+    var arr = [];
+    for (var key in object){
+        arr.push(key)
+    }
+    return arr.join(" ");
+}
+
+
+
+function valuesToString(object) {
+    var arr = [];
+    for (var key in object) {
+        arr.push(object[key]);
+    }
+    return arr.join(" ");
 }
